664b75653a10359ddfae7be0d8ccae7f5fff0c6a
Clara Jimenez <clarajimenezrecio@gmail.com>
2015-06-02 17:52:26 +0200

    Tabla de usuarios + Cambios estadísticas

--- controllers/statistics_controller.js
+++ controllers/statistics_controller.js
@@ -6,25 +6,41 @@ exports.statistics = function(req, res){
     var numPregs = quizes.length;
     models.Comment.findAll().then(function(comments){
       var numComentarios = comments.length;
-      var media = numComentarios/numPregs;
+      var numComentariospublis = comments.length;
+      var numPregsTot = 0;
+      var media = 0;
       var sinComments = 0;
       var conComments = 0;
       var array=[];
+      var borrado=1;
+      for(var i=0; i<numComentarios; i++){ // por si se borran preguntas (en este caso numPregs no coincidirá con las dimensiones reales de la tabla, si no con las preguntas no nulas)
+        if (comments[i].QuizId > numPregsTot) numPregsTot=comments[i].QuizId;
+      }
       for(var i=0; i<numComentarios; i++){ // recorro la tabla de comentarios y meto en cada índice del array[i] cuantos comentarios tiene la pregunta número i 
-        if(array[comments[i].QuizId]){ // si no es el primer comentario con ese QuizId, aumento en 1 array[QuizId]
-          array[comments[i].QuizId]++;
-        }else{ // si es el primer comentario con ese QuizId, pongo a 1 array[QuizId]
-          array[comments[i].QuizId] = 1;
+        for (var j=0; j<numPregs; j++) {
+          if (quizes[j].id==comments[i].QuizId) {
+            borrado=0;
+          }
+        }
+        if (comments[i].publicado==1 && borrado==0) { // compruebo que no esté borrada la pregunta y que el comentario esté publicado
+          if(array[comments[i].QuizId]){ // si no es el primer comentario con ese QuizId, aumento en 1 array[QuizId]
+            array[comments[i].QuizId]++;
+          }else{ // si es el primer comentario con ese QuizId, pongo a 1 array[QuizId]
+            array[comments[i].QuizId] = 1;
+          }
+        }
+        else {
+          numComentariospublis--;
         }
       }
-      for(var i=1; i<=numPregs; i++){ // recorro la tabla de preguntas
+      for(var i=1; i<=numPregsTot; i++){ // recorro la tabla preguntas
         if(array[i]){ // si había comentarios en esa pregunta (es decir, guardé un array[i]!=undefined), conComments++
           conComments++;
-        } else{
-          sinComments++;
         }
       }
-      res.render('quizes/statistics', {quizes: quizes, preguntas: numPregs, comentarios: numComentarios, media: media, sinComments: sinComments, conComments: conComments, errors: []});
+      sinComments = numPregs-conComments;
+      media = numComentariospublis/numPregs;
+      res.render('quizes/statistics', {quizes: quizes, preguntas: numPregs, comentarios: numComentariospublis, media: media, sinComments: sinComments, conComments: conComments, errors: []});
     }).catch(function(error){next(error);})
   }).catch(function(error){next(error);})
 };
--- models/models.js
+++ models/models.js
@@ -1,39 +1,30 @@
 var path = require('path');
 
-
-//Postgress DATABAS_URL = postgress://user:passwd@host:port/database
-//Sqlite DATABASE_URL = sqlite://:@:/
+// Postgres DATABASE_URL = postgres://user:passwd@host:port/database
+// SQLite   DATABASE_URL = sqlite://:@:/
 var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
-var DB_name = (url[6] || null);
-var user  = (url[2] || null);
-var pwd  = (url[3] || null);
-var protocol  = (url[1] || null);
+var DB_name  = (url[6]||null);
+var user     = (url[2]||null);
+var pwd      = (url[3]||null);
+var protocol = (url[1]||null);
+var dialect  = (url[1]||null);
+var port     = (url[5]||null);
+var host     = (url[4]||null);
+var storage  = process.env.DATABASE_STORAGE;
 
-//******************************************************
-//cambio el dialecto manualmente
-var dialect = "sqlite";
-//*******************************************************
-var port = (url[5] || null);
-var host = (url[4] || null);
-var storage = process.env.DATABASE_STORAGE;
-
-//CARGAR modelo ORM
+// Cargar Modelo ORM
 var Sequelize = require('sequelize');
 
-//USAR BBDD SQlite o  Postgres
-var sequelize = new Sequelize(DB_name,user,pwd,
-                        {
-//*****************************************************
-//llamo al dialecto puesto manualmente
-dialect: dialect,
-//*****************************************************
-                        protocol: protocol,
-                        port: port,
-                        host: host,
-                        storage: storage,
-                        omitNull: true
-                    }
-                );
+// Usar BBDD SQLite o Postgres
+var sequelize = new Sequelize(DB_name, user, pwd, 
+  { dialect:  protocol,
+    protocol: protocol,
+    port:     port,
+    host:     host,
+    storage:  storage,  // solo SQLite (.env)
+    omitNull: true      // solo Postgres
+  }      
+);
 
 // Importar definicion de la tabla Quiz
 var quiz_path = path.join(__dirname,'quiz');
@@ -43,22 +34,43 @@ var Quiz = sequelize.import(quiz_path);
 var comment_path = path.join(__dirname,'comment');
 var Comment = sequelize.import(comment_path);
 
+// Importar definicion de la tabla Comment
+var user_path = path.join(__dirname,'user');
+var User = sequelize.import(user_path);
+
 Comment.belongsTo(Quiz);
 Quiz.hasMany(Comment);
 
-exports.Quiz = Quiz; //exportar definicion de la tabla Quiz
-exports.Comment = Comment;
+// los quizes pertenecen a un usuario registrado
+Quiz.belongsTo(User);
+User.hasMany(Quiz);
 
-//sequelize.sync() crea e inicializa la tabla de preguntas en DB
+// exportar tablas
+exports.Quiz = Quiz; 
+exports.Comment = Comment; 
+exports.User = User;
+
+// sequelize.sync() inicializa tabla de preguntas en DB
 sequelize.sync().then(function() {
-    //then(..) ejecuta el manejador una vez creada la tabla
-    Quiz.count().then(function(count){
-        if(count === 0) { //la tabla se inicializa solo si esta vacia
+  // then(..) ejecuta el manejador una vez creada la tabla
+  User.count().then(function (count){
+    if(count === 0) {   // la tabla se inicializa solo si está vacía
+      User.bulkCreate( 
+        [ {username: 'admin',   password: '1234', isAdmin: true},
+          {username: 'pepe',   password: '5678'} // el valor por defecto de isAdmin es 'false'
+        ]
+      ).then(function(){
+        console.log('Base de datos (tabla user) inicializada');
+        Quiz.count().then(function (count){
+          if(count === 0) {   // la tabla se inicializa solo si está vacía
             Quiz.bulkCreate( 
-                [ {pregunta: 'Capital de Italia',   respuesta: 'Roma'},
-                  {pregunta: 'Capital de Portugal', respuesta: 'Lisboa'}
-                ]
-              ).then(function(){console.log('Base de datos inicializada')});
-        };
-    });
+              [ {pregunta: 'Capital de Italia',   respuesta: 'Roma', UserId: 2}, // estos quizes pertenecen al usuario pepe (2)
+                {pregunta: 'Capital de Portugal', respuesta: 'Lisboa', UserId: 2}
+              ]
+            ).then(function(){console.log('Base de datos (tabla quiz) inicializada')});
+          };
+        });
+      });
+    };
+  });
 });
